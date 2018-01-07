12fb42097f34016be7ea3bb3ab0c55cb0eda253a
Kateri Casey <casey1km@gmail.com>
2018-01-07 23:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,25 @@ function logWhisper(hello) {
   console.log(hello.toLowerCase())
 }
 function sayHiToGrandma(hello) {
+<<<<<<< HEAD
   if (hello === hello.toLowerCase())
     return "I can't hear you!";
   else if (hello === hello.toUpperCase())
     return "YES INDEED!";
   else
     return "I love you, too.";
+=======
+  if (hello.toLowerCase())
+    return "I can't hear you!"
+}
+function sayHiToGrandma(hello) {
+  if(hello.toUpperCase())
+  return "YES INDEED!"
+}
+function sayHiToGrandma(hello) {
+  if("I love you, Grandma")
+  return "I love you, too"
+}
+
+>>>>>>> 3e519048839821a2b67eb84bdc0e84f1a829bcda
 }
